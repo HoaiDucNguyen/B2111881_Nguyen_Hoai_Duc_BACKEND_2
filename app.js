e81709cd49471be8329c42320aad739cfa452df4
HoaiDuc <ducb2111881@student.ctu.edu.vn>
2024-09-09 13:26:49 +0700

    Cai at xu ly loi

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +16,14 @@ app.get("/",(req, res) => {
     });
 });
 app.use("/api/contacts", contactRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+     res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    })
+})
 
 module.exports = app;
